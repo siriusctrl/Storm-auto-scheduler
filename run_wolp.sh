7ff9d4545d5db1724df8ca6cf5a151043ba8e78e
Xinyao Niu <morryniu123@gmail.com>
2021-09-19 02:11:15 +1000

    update training scripts

--- run_wolp.sh
+++ run_wolp.sh
@@ -1,1 +1,1 @@
-python main_wolpertinger_parallel.py --n_env=15 --model=li --max_timesteps=6000 --start_timesteps=600 --seed=10 --max_episodic_length=10000 --extra_name=small
+python main_wolpertinger_parallel.py --n_env=10 --model=li --max_timesteps=6000 --start_timesteps=0 --seed=10 --max_episodic_length=10000 --extra_name=small
